5e108bcf664b609ace569999a49dd2ec2ef4cea8
Luigi-V <vollonoluigi@hotmail.com>
2021-06-23 09:33:32 +0200

    Update Consistance_Test.py

--- Consistance_Test/Consistance_Test.py
+++ Consistance_Test/Consistance_Test.py
@@ -5,35 +5,22 @@ import time
 
 ### CONNESSIONE A MONGODB SERVER 
 client = MongoClient("mongodb+srv://luigi:luigi@vgsales.x71p0.mongodb.net/test?authSource=admin&replicaSet=atlas-3sr7cm-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
-"""
-### CONNECTION TEST
-# Issue the serverStatus command and print the results
-serverStatusResult=(client.admin).command("serverStatus")
-pprint(serverStatusResult)
-"""
-
-### VERIFICA DELLA CONSISTENZA DEI CAMPI NEI DOCUMENTI
 # connesione al db "vgsales"
 db = client.vgsales
 # restituisce tutti i videogiochi (documenti) non consistenti
-query = {"$or": [{"Name": "N/A"}, {"Platform": "N/A"}, {"Year": float("NaN")}, {"Genre": "N/A"}, 
-                {"Publisher": "N/A"}, {"NA_Sales": float("NaN")}, {"EU_Sales": float("NaN")}, 
+query = {"$or": [{"Name": "N/A"}, {"Platform": "N/A"}, {"Year": float("NaN")}, {"Genre": "N/A"}, {"Publisher": "N/A"}, {"NA_Sales": float("NaN")}, {"EU_Sales": float("NaN")}, 
                 {"JP_Sales": float("NaN")}, {"Other_Sales": float("NaN")}, {"Global_Sales": float("NaN")}]}
-
-
 n_games = db.main_data.count_documents(query)
 print("\n-Videogiochi non consistenti: "+ str(n_games)+ "\n")
-
 if n_games > 0:
     games = db.main_data.find(query)    
     try:
         for game in games:
             game = dict(game)
-            print(game)
-
+            print(game)            
         # Rank è la chiave primaria del documento
             id_rank = {"Rank": game.get("Rank")}
-
+            
         # controlli per l'eliminazione degli attributi nulli
             if game.get("Name") == "N/A":
                 del game["Name"]
@@ -69,14 +56,19 @@ if n_games > 0:
             print("\n")
 
     except:
-        #print("\n\n")
         raise Exception()
-
     games.close()
 
 else:
     print("\n-Tutti i videogiochi sono consistenti...")
 
 print("\n-Videogiochi non consistenti: "+ str(n_games)+ "\n")
-
 print("\n")
+
+
+"""
+### CONNECTION TEST
+# Issue the serverStatus command and print the results
+serverStatusResult=(client.admin).command("serverStatus")
+pprint(serverStatusResult)
+"""
